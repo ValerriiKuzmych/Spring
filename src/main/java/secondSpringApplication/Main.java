9c08212cb99cd8ded599d99b0253f4d0c4307d41
Valerii Kuzmych <kuzmychvalerii@gmail.com>
2024-01-30 17:05:01 +0100

    Update: added dependency injection. (second Spring app, only with XML realisation.)

--- src/main/java/secondSpringApplication/Main.java
+++ src/main/java/secondSpringApplication/Main.java
@@ -7,11 +7,12 @@ public static void main(String[] args) {
 	
 	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 	
-	Music music = context.getBean("musicBean", Music.class);
+	//Music music = context.getBean("musicBean", Music.class);
 	
-	MusicPlayer player = new MusicPlayer(music);
+	//MusicPlayer player = new MusicPlayer(music);
 	
 	
+	MusicPlayer player =  context.getBean("musicPlayerBean", MusicPlayer.class);
 	player.playMusic();
 	
 	context.close();
